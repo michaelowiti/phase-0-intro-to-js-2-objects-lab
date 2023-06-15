810e597704ffc7bb738b4088b23520cba7524a97
mike <mjowiti@gmail.com>
2023-06-15 18:10:35 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee ={ name: "lolita",
+                  streetAddress:"newJersey"};
+
+function updateEmployeeWithKeyAndValue(employee,object,key,value){
+const newEmployee={...employee,
+                     name:"Sam",
+                    streetAddress:"11 Broadway"};
+
+    return newEmployee
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    employee.streetAddress="12 Broadway"
+               
+    return employee
+}
+function deleteFromEmployeeByKey(employee,key){
+    
+    const newObject= {...employee,}
+        delete newObject.name;
+    return newObject
+}
+function destructivelyDeleteFromEmployeeByKey(){
+    delete employee.name;
+    return employee
+}
